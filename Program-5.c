8258e9bcf6858244f718b2d8c3cc6a73f0f5f64b
pR916 <20me0114@vvce.ac.in>
2021-03-08 12:39:43 +0000

    Yes

--- Program-5.c
+++ Program-5.c
@@ -7,15 +7,13 @@ int main()
 {
    int a[10],i,sum =0, n;
    printf("Enter number of elements in array and the array elements");
-   scanf("%d", &n);
- printf("enter the elements of the array");
   // printf("Enter %d elements\n", n);
- 
+   scanf("%d",&n);
    for (i = 0; i < n; i++)
    {
       scanf("%d", &a[i]);
    }
-printf("enter %d elements \n",n);
+
  for (i=0; i<n;i++)
    {
       sum=sum+a[i]; 
